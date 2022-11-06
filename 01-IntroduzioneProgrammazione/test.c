8b68c86039ea2a8c58ce98aa16bcee0c9432e301
dadezz <dadezz@protonmail.com>
2022-11-06 02:11:14 +0000

    prova di inserimento lab utente

--- 01-IntroduzioneProgrammazione/test.c
+++ 01-IntroduzioneProgrammazione/test.c
@@ -1,15 +1,35 @@
 #include<stdio.h>
 #include "test_ext.c"
+#include<stdlib.h>
 
-#define lunghezza 4
-#define lunghezza1 4
 
-extern void f(int array[lunghezza1][lunghezza]);
-int main(){
-    int prova[][lunghezza] = {{1,2,3,4},{1,2,3,4},{1,2,3,4},{1,2,3,4}};
 
-    f(prova);
 
+
+extern void scan(int *righe , int *colonne);
+extern void inserimento(char* vet, int *righe, int *colonne);
+extern void print(char* vet , int *righe , int *colonne);
+
+int  main(){
+    /*int prova[][lunghezza] = {1,2,3,4,
+                            1,2,3,4,
+                            1,2,3,4,
+                            1,2,3,4};
+    */
+    
+    int righe;
+    int colonne;
+    scan(&righe,&colonne);
+
+    char* prova = (char*) malloc((righe*colonne)*sizeof(char));
+    printf(" righe casso %d\n",righe);
+    printf(" colonne minchia %d\n",colonne);
+    if(prova != NULL) printf("ok \n");
+    if(prova == NULL) return 1;
+
+    inserimento(prova,&righe,&colonne);
+    print(prova, &righe, &colonne);
+     
     return 0;
 
 }
--- 01-IntroduzioneProgrammazione/test_ext.c
+++ 01-IntroduzioneProgrammazione/test_ext.c
@@ -1,10 +1,64 @@
 #include<stdio.h>
-#define lunghezza 4
-#define lunghezza1 4
 
-void f(int array[lunghezza1][lunghezza]){
-    for (int i =0, j=0; i<lunghezza; i++){
-        printf("%d", array[i][j++]);
+void scan(int *righe , int *colonne){
+    scanf("%d",righe);
+    scanf("%d",colonne);
+}
+
+
+void inserimento(char* vet, int *righe, int *colonne){//creazione campo/inserimento
+    //int pos = 1;
+    
+    for (size_t j = 0; j < *righe; ++j){
+        size_t i=0;
+        //for(; i<*colonne+1 && (vet[i+j*(*colonne)]=getchar())!='\n' ;i++);
+        //if (vet[i] != '\n') break;
+        for(;i<*colonne;i++){
+            //vet[(i + j*(*colonne))] = getchar();
+            while ((vet[(i + j*(*colonne))] = getchar())=='\n'); //cumme cazz iè pussibbile?
+            //vet[(i + j*(*colonne))] = getchar();
+            
+        }
+
     }
 }
 
+/*
+  i + j*colonne
+  
+  j = 0:
+    0 + 0*4
+    1 + 0*4
+    2 + 0*4
+    3 + 0*4
+  j = 1
+    0 + 1*4 4
+    1 + 1*4 5
+    2 + 1*4 6
+    3 + 1*4 7
+  j = 2:
+    0 + 2*4 8
+    1 + 2*4 9
+    2 + 2*4 10
+    3 + 2*4 11
+    
+
+
+*/
+
+void print(char* vet , int *righe , int *colonne){
+
+      for (size_t i = 0; i < (*righe) * (*colonne); i++)
+      {
+       // printf("%c %d\n",vet[i],i);
+
+         if (vet[i] != '\n')
+         {
+          printf("%c %d\n",vet[i],i);
+
+         }
+          
+        
+      }
+      
+}
