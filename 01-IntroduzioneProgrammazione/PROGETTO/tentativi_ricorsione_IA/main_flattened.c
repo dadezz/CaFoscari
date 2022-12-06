035418ead37dec01f3930f9cc3ef890f42531652
dadezz <dadezz@protonmail.com>
2022-12-06 15:17:37 +0000

    tentativo flattening, problemi con malloc (?)

--- 01-IntroduzioneProgrammazione/PROGETTO/tentativi_ricorsione_IA/main_flattened.c
+++ 01-IntroduzioneProgrammazione/PROGETTO/tentativi_ricorsione_IA/main_flattened.c
@@ -2,16 +2,16 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<unistd.h>
-#define DEBUG 0
+#define DEBUG 1
 
-int max_p = 0; //variabile globale col punteggio migliore
+int max_p = 0;//variabile globale col punteggio migliore
 
 struct board{
     /**
      * array bidimensionale per la mappa
      * numero di righe e colonne
     **/
-    char** map;
+    char* map;
     int row;
     int col;
 };
@@ -23,16 +23,7 @@ struct vector{
     int* data;
     size_t size;
 };
-struct position_vector{
-    /**
-     * vettore di posizioni:
-     * si porta dietro i due array per le due coordinate
-     * e la dimensione
-    */
-    int* col;
-    int* row;
-    size_t size;
-};
+
 struct position{
     /**
      * punto sulla mappa
@@ -43,17 +34,14 @@ struct position{
 };
 typedef struct board board_t;
 typedef struct vector vector_t;
-typedef struct position_vector vector_t;
 typedef struct position position_t;
 
-void stampa_direzioni(vector_t p){
+void stampa_direzioni(vector_t p, board_t map){
     for (int i=1; i<p.size; i++){
-        if (p.col[i] == p.col[i-1]){
-            if (p.row[i] - p.row[i-1] == 1) printf("S ");
-            else printf("N ");
-        }
-        else if(p.col[i] - p.col[i-1] == 1) printf("E ");
-        else printf("O ");
+        if (p.data[i] - p.data[i-1] == 1) printf("E ");
+        if (p.data[i] - p.data[i-1] == -1) printf("O ");
+        if (p.data[i] - p.data[i-1] == map.col) printf("S ");
+        else printf("N ");
     }
 }
 position_t find_char(board_t map, char c){
@@ -63,7 +51,7 @@ position_t find_char(board_t map, char c){
     bool controllo = false;
     for (int i=0; i<map.row; i++){
         for (int j=0; j<map.col; j++){
-            if (map.map[i][j] == c) {
+            if (map.map[i*map.col + j] == c) {
                 p.row = i;
                 p.col = j;
                 controllo = true;
@@ -89,12 +77,13 @@ int max(vector_t x){
     for (int i=1; i<x.size; i++) if (x.data[i]>max) max=x.data[i];
     return max;
 }
-bool not_in(vector_t percorso, position_t pos){
+bool not_in(vector_t percorso, position_t pos, board_t map){
     //controllo se sono già passato sull'attuale posizione
     //( => no loop infiniti )
+    
     bool a = true;
-    for (int i=0; i<percorso.size && a; i++) 
-        if (percorso.row[i] == pos.row && percorso.col[i] == pos.col) 
+    for (int i=0; i<percorso.size && a; i++)
+        if (percorso.data[i] == (pos.row*map.col + pos.col))
             a = false;
     return a;
 }
@@ -105,7 +94,7 @@ void stampa_piu(board_t map, vector_t percorso){
             position_t pos;
             pos.row = i;
             pos.col = j;
-            if (not_in(percorso, pos)) printf("%c ", map.map[i][j]);
+            if (not_in(percorso, pos, map)) printf("%c ", map.map[i*map.row + j]);
             else printf("+ ");
         }
         printf("\n");
@@ -113,15 +102,14 @@ void stampa_piu(board_t map, vector_t percorso){
 }
 bool punto_valido(board_t map, position_t p, vector_t percorso, int T){
     //un punto è valido se non ci sono passato sopra prima e se non è un muro    
-    if (T==0) return map.map[p.row][p.col] != '#' && not_in(percorso, p);
-    else return not_in(percorso, p);
+    if (T==0) return map.map[p.row*map.col + p.col] != '#' && not_in(percorso, p, map);
+    else return not_in(percorso, p, map);
 }
 void elimina_piu(vector_t percorso, int x){
     //quando torno indietro, elimino gli ultimi passi che ho fatto dall'array, sovrascrivendoli con zeri
     int s = percorso.size;
     for (int i = x+1; i<s; i++) {
-        percorso.col[i] = 0;
-        percorso.row[i] = 0;
+        percorso.data[i] = 0;
     }
 }
 void scambia_punteggio_max(int punteggio, vector_t path, vector_t percorso){
@@ -130,8 +118,7 @@ void scambia_punteggio_max(int punteggio, vector_t path, vector_t percorso){
     if (punteggio > max_p){
         max_p = punteggio;
         for (int i = 0; i<percorso.size; i++) {
-            path.col[i] = percorso.col[i];
-            path.row[i] = percorso.row[i];
+            path.data[i] = percorso.data[i];
         }
     }
 }
@@ -140,17 +127,17 @@ int best_percorso(board_t map, vector_t percorso, vector_t path, position_t posi
     #if DEBUG
     sleep(1);
     stampa_piu(map, percorso);
-    printf("position: (%d, %d)\n", position.row, position.col);
+    printf("position: %d\n", position.row*map.col + position.col);
     printf("max: %d ", max_p);
     printf("[ ");
     for(int i=0; i<path.size; i++)
-        printf(" (%d, %d) ", path.row[i], path.col[i]); 
+        printf(" %d ", path.data[i]); 
     printf(" ]\n");
     printf("size vett percorso: %d\n", percorso.size);
     printf("i: %d\n", contatore);
     printf("[ ");
     for(int j=0; j<percorso.size; j++) 
-        printf(" (%d, %d) ", percorso.row[j], percorso.col[j]);
+        printf(" %d", percorso.data[j]);
     printf(" ]\n");
     printf("numero bonus: %d\n", bonus);
     #endif
@@ -158,12 +145,11 @@ int best_percorso(board_t map, vector_t percorso, vector_t path, position_t posi
     //Caso base
     if (position.col == fine.col && position.row == fine.row) return 1000-contatore+10*bonus;
     if (punto_valido(map, position, percorso, T)){
-        if (map.map[position.row][position.col] == '$') bonus++;
-        if (map.map[position.row][position.col] == '!') bonus = bonus/2;
-        if (map.map[position.row][position.col] == 'T') T+=3;
-        if (map.map[position.row][position.col] == '#') T-=1;
-        percorso.col[contatore] = position.col;
-        percorso.row[contatore] = position.row;
+        if (map.map[position.row*map.col + position.col] == '$') bonus++;
+        if (map.map[position.row*map.col + position.col] == '!') bonus = bonus/2;
+        if (map.map[position.row*map.col + position.col] == 'T') T+=3;
+        if (map.map[position.row*map.col + position.col] == '#') T-=1;
+        percorso.data[contatore] = position.row*map.col + position.col;
         vector_t punteggi;
         punteggi.size = 4;
         punteggi.data = calloc(sizeof(int), punteggi.size);
@@ -229,21 +215,17 @@ int main(){
     // inizializzazione con valori di default, nel programma POSSONO essere presi in input 
     default_map.row = 10;
     default_map.col = 6;
-    default_map.map = (char**) malloc((sizeof(char*) * default_map.row));
+    default_map.map = (char*) malloc((sizeof(char) * default_map.row));
     if (default_map.map == NULL) errore();
-    for(int i=0; i<default_map.row; i++){
-        default_map.map[i] = (char*) malloc(sizeof(char)* default_map.col);
-        if (default_map.map[i] == NULL) errore ();
-        for (int j=0; j<default_map.col; j++) {
-            default_map.map[i][j]= mappa_di_prova[j+i*default_map.col];
-        }   
+    for(int i=0; i<default_map.row*default_map.col; i++){
+        default_map.map[i]= mappa_di_prova[i];
     }
 
     //trovo inizio e fine. se fallisco, libero memoria allocata e esco dal programma
     position_t inizio = find_char(default_map, 'o');
     if (inizio.col == -1) {
         for (int i=0; i<default_map.row; i++){
-            free(default_map.map[i]);
+            free(default_map.map);
         }
         free(default_map.map);
         printf("inizio non trovato\n");
@@ -252,7 +234,7 @@ int main(){
     position_t fine = find_char(default_map, '_');
     if (fine.col == -1) {
         for (int i=0; i<default_map.row; i++){
-            free(default_map.map[i]);
+            free(default_map.map);
         }
         free(default_map.map);
         printf("fine non trovato\n");
@@ -260,8 +242,10 @@ int main(){
     }
 
     #if DEBUG
-    printf("inizio: (%d, %d)\n", inizio.row, inizio.col);
-    printf("fine: (%d, %d)\n", fine.row, fine.col);
+    printf("%d", inizio.row);
+    int posizione = inizio.row*default_map.col + inizio.col;
+    printf("inizio: (%d, %d), flattened: %d\n", inizio.row, inizio.col, posizione);
+    printf("fine: (%d, %d), flattened: %d\n", fine.row, fine.col, fine.row*default_map.col + fine.col);
     #endif
     
 
@@ -269,55 +253,47 @@ int main(){
     //(quindi cambia a ogni chiamata di funzione ricorsiva)
     vector_t percorso;
     percorso.size = default_map.row * default_map.col; //massima lunghezza possibile
-    percorso.col = (int*)calloc(percorso.size, sizeof(int));
-    if (percorso.col == NULL) errore ();
-    percorso.row = (int*)calloc(percorso.size, sizeof(int));
-    if (percorso.row == NULL) errore ();
+    percorso.data = (int*)calloc(percorso.size, sizeof(int));
+    if (percorso.data == NULL) errore ();
 
     //path è l'array con il percorso definitivo.
     //nella funzione, quando viene trovato un percorso con punteggio maggiore del 
     //precedente, path assume i valori di percorso corrispondente
     vector_t path;
     path.size = default_map.row * default_map.col; //massima lunghezza possibile
-    path.col = (int*)calloc(path.size, sizeof(int));
-    if (path.col == NULL) errore ();
-    path.row = (int*)calloc(path.size, sizeof(int));
-    if (path.row == NULL) errore ();
+    path.data = (int*)calloc(path.size, sizeof(int));
+    if (path.data == NULL) errore ();
     
     //chiamata di funzione
     best_percorso(default_map, percorso, path, inizio, fine, 0, 0, 0);
     printf("\n miglior punteggio: %d\n", max_p);
     
     //libero percorso, non mi serve più
-    free(percorso.col);
-    free(percorso.row);
+    free(percorso.data);
 
     //calcolo la lunghezza effettiva del path e realloco di conseguenza
     bool f = false;
     size_t size = 0;
     for (int j=0; j<path.size && !f; j++) {
         size++;
-        if (path.col[j] == 0 && path.row[j] == 0) break;
+        if (path.data[j] == 0) break;
     };
     path.size = size;
-    path.col[size-1] = fine.col;
-    path.row[size-1] = fine.row;
-    path.col = realloc(path.col, path.size*sizeof(int));
-    if (path.col == NULL) errore ();
-    path.row = realloc(path.row, path.size*sizeof(int));
-    if (path.row == NULL) errore ();
+    path.data[size-1] = fine.col + fine.row*default_map.col;
+    path.data = realloc(path.data, path.size*sizeof(int));
+    if (path.data == NULL) errore ();
 
     //stampo le posizioni
     printf("percorso:   ");
     for(int i=0; i<size; i++) {
-        if (i!=size-1) printf("(%d, %d) -> ", path.row[i], path.col[i]);
-        else printf("(%d, %d)", path.row[i], path.col[i]);
+        if (i!=size-1) printf("%d -> ", path.data[i]);
+        else printf("%d", path.data[i]);
     }
     printf("\n");
 
     //stampo le mosse
     printf("mosse effettuate:   ");
-    stampa_direzioni(path);
+    stampa_direzioni(path, default_map);
     printf("\n");
 
 
@@ -325,10 +301,9 @@ int main(){
     stampa_piu(default_map, path);
     
     //libero il resto
-    free(path.col);
-    free(path.row);
+    free(path.data);
     for (int i=0; i<default_map.row; i++){
-        free(default_map.map[i]);
+        free(default_map.map);
     }
     free(default_map.map);
     return 0;
