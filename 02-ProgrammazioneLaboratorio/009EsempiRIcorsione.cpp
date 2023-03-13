78108d3adb6e7282be4574a0443c52078aaea405
dadezz <dadezz@protonmail.com>
2023-03-13 21:40:18 +0100

    due esercizi di ricorsione

--- 02-ProgrammazioneLaboratorio/009EsempiRIcorsione.cpp
+++ 02-ProgrammazioneLaboratorio/009EsempiRIcorsione.cpp
@@ -49,11 +49,10 @@ void sort(int* a, int n){
 string bin(uint64_t n){
     if (n==0) return "0";
     if (n==1) return "1";
-
     return bin(n/2) + (n%2 ? string{"1"} : string{"0"});
 }
 
-int main(){
+int main0(){
     int a[] = {8, -1, 0, 17, 19, 2};
     int dim = sizeof(a)/sizeof(int);
 
@@ -68,8 +67,79 @@ int main(){
     string s;
     s = bin(n);
     cout << s << endl;
+    return 0;
 }
 
 /**
  * next time we'll see some example where recursive functs calls others recursive functs and viceversa.
 */
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Exercises
+
+/**
+ * Il massimo comune divisore MCD(a,b) tra due interi a <= b si calcola ricorsivamente 
+ * con l'algoritmo di Euclide come segue: se a == 0, restituisci b. Altrimenti, 
+ * restituisci MCD(b-a,a). Scrivere una funzione ricorsiva per calcolare MCD.
+*/
+
+void scambia (int & a, int & b){
+    int aux = a;
+    a = b;
+    b = aux;
+}
+
+int mcd(int a, int b){
+    if (a>b) scambia(a, b);
+    if (a == 0) return b;
+    else return mcd(b-a, a);
+}
+
+/**
+ * La edit distance ED(x,y) tra due stringhe x,y è il numero minimo di sostituzioni, 
+ * inserimenti, cancellazioni (di un carattere singolo) che dobbiamo effettuare su x 
+ * per trasformarla in y. Per esempio, ED("sunday" , "saturday") = 3 perchè possiamo 
+ * trasformare "sunday" in "saturday" con tre modifiche: prima, inserisco due 
+ * caratteri "at", ottenendo "satunday"; infine, rimpiazzo la 'n' in 'r', ottenendo 
+ * "saturday". In tutto 3 modifiche: due caratteri inseriti e un carattere sostituito 
+ * (ci possono essere diversi modi di effettuare il numero minimo di modifiche; a noi 
+ * interessa solo calcolare il numero minimo di modifiche). Altro esempio: ED("abc" , 
+ * "ac") = 1, perchè basta eliminare 'b' da "abc" per ottenere "ac". Scrivere un 
+ * programma ricorsivo per calcolare ED(x,y). Nota: il corpo della funzione si scrive
+ * in 6 linee di codice! evitate soluzioni troppo complicate.
+*/
+
+#include <cstring>
+
+int min(int a, int b, int c){
+    int min = a;
+    if (b < min) min = b;
+    if (c < min) min = c;
+    return min;
+}
+
+int distance(char* x, char* y){
+    if (strlen(x) == 1 && strlen(y) == 1 && x[0] == y[0]) return 0; //caso base str uguali
+    if (strlen(x) == 1 && strlen(y) == 1 && x[0] != y[0]) return 1; //caso base distanza 1 (rimpiazzi)
+    if (strlen(x) == 0 && strlen(y) > 0) return strlen(y);  //caso base inserimento lettere
+    if (strlen(y) == 0 && strlen(x) > 0) return strlen(x);
+    if (x[0] == y[0]) return distance(x+1, y+1);    //se i primi due caratteri sono uguali, posso ridurre il prooblema al resto della stringa
+    /**
+     * tip: non bisogna guardare COSA cambiare, ma QUANTO. non serve capire quali lettere aggiungere, ma solo quante
+     * come nel labirinto snake, posso ritornare il minimo tra le tre vie possibili: aggiunta, sostituzione e rimozione
+    */
+    return 1 + min(
+        distance(x+1, y),   //sto rimovendo un carattere da x (lo shifto in avanti).
+        distance(x+1, y+1), //chissenefrega di cosa e come cambio, se cambio qualcosa posso spostarmi ad analizzare la rimanente parte.
+        distance(x, y+1)    //sto aggiungendo un carattere a x (perché shifto di uno y)
+    );   
+}
+
+int main(){
+    cout<<mcd(8, 64)<<endl;
+    char a[] = "saturday";
+    char b[] = "sunday";
+    cout<<"quanti cambi: "<<distance(a, b)<<endl;
+    return 0;
+}
