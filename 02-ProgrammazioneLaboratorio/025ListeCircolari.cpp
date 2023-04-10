2c502fbe0a930d15015ffff42693d5acc4d14690
dadezz <dadezz@protonmail.com>
2023-04-11 00:36:21 +0200

    v

--- 02-ProgrammazioneLaboratorio/025ListeCircolari.cpp
+++ 02-ProgrammazioneLaboratorio/025ListeCircolari.cpp
@@ -1,4 +1,22 @@
 /**
  * nessun puntatore a next punta a nullptr, l' "ultima" punta alla prima
- * 
+ * Notiamo che il tipo è indistinguibile da una linked list normale.
 */
+
+#include<iostream>
+
+struct cella {
+    int info;
+    cella* next;
+};
+typedef cella* Listacirc;
+
+void print(Listacirc l){
+    if (l){
+        Listacirc k = l;
+        do {
+            l=l->next;
+            std::cout<<l->info;
+        } while(l != k);
+    }
+}
