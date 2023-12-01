795a1f59739a4e0efa50f76f86b918f8172d719d
dadezz <davidezambon1112@gmail.com>
2023-12-01 02:05:49 +0100

    esercizio 3 penso giusto, idk complessita

--- 06 - Algoritmi e Strutture Dati/Alberi/Prima_Esercitazione.cpp
+++ 06 - Algoritmi e Strutture Dati/Alberi/Prima_Esercitazione.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <algorithm>
+#include <iostream>
 /////////////////////////////////
     // ESERCIZIO 1
 /////////////////////////////////
@@ -11,8 +12,8 @@ Un albero binario si dice t-bilanciato se per ogni suo nodo vale la proprietà: 
     Discutere la complessità della soluzione trovata.
 
 Il prototipo della funzione è:
-*/
 int tBil(PNode u);
+*/
 /*
 Analizzare e motivare in modo chiaro, preciso ed approfondito la complessità della funzione.
 
@@ -65,8 +66,8 @@ Sia T un albero generale i cui nodi hanno campi: key, left-child e right-sib. Si
 per ogni nodo u, le chiavi dei figli del nodo u devono avere valori non decrescenti considerando i figli di u da sinistra verso destra.
 
 Il prototipo della funzione è:
-*/
 bool isNonDec(PNodeG r);
+*/
 /*
 Restituisce true se la proprietà è verificata altrimenti false.
 
@@ -212,9 +213,7 @@ PNode ricostruisci(const std::vector<int>& va, const std::vector<int>& vs){
 Dato un albero binario, scrivere una funzione che costruisce un array bidimensionale mat tale che, per ogni coppia di nodi u e v, 
 l’elemento mat[u][v] sia il minimo antenato comune di u e v. La funzione deve richiedere tempo O(n^2) e il suo prototipo è:
 */
-void minAntCom(PNode r, std::vector<std::vector<int>>& mat){
-
-}
+void minAntCom(PNode r, std::vector<std::vector<int>>& mat);
 /*
 Per semplicità si assuma che i nodi abbiano chiavi numerate da 0 a n-1 e che queste chiavi identifichino il nodo. 
 Di conseguenza gli indici della matrice rappresentano i nodi e in posizione mat[u][v] c'è la chiave del nodo che è il minimo antenato comune di u e v.
@@ -272,3 +271,83 @@ per ogni nodo visitato, c'è una relazione tra lui come indice primario e tutti 
 Si noti che non è che ogni nodo viene visitato una volta e si relaziona con tutti in una volta sola, ma a ogni visita si relaziona con nodi diversi dell'albero
                   
 */
+void minAntCom_aux(const PNode root, const PNode v, const PNode u, std::vector<std::vector<int>>& mat){
+    if (u == nullptr) return;
+    mat[v->key][u->key] = root->key;
+    mat[u->key][v->key] = root->key;
+    minAntCom_aux(root, v, u->left, mat);
+    minAntCom_aux(root, v, u->right, mat);
+
+    if (v->left) minAntCom_aux(root, v->left, u, mat);
+    if (v->right) minAntCom_aux(root, v->right, u, mat);
+
+}
+void minAntCom(PNode r, std::vector<std::vector<int>>& mat){
+    if (r == nullptr) return;
+
+    // populates mat
+    mat[r->key][r->key] = r->key;
+    minAntCom_aux(r, r, r->left, mat);
+    minAntCom_aux(r, r, r->right, mat);
+
+    //recursion
+    minAntCom(r->left, mat);
+    minAntCom(r->right, mat);    
+}
+
+int main() {
+    // Crea un semplice albero binario per il test:
+    /*
+              3
+            /   \
+          1       4
+        /  \       \
+      2     0       5
+    
+    */
+    PNode root = new Node {
+        3, 
+        new Node{
+            1,
+            new Node{
+                2, 
+                nullptr, 
+                nullptr
+            },
+            new Node{
+                0, 
+                nullptr, 
+                nullptr
+            }
+        },
+        nullptr
+    };
+    
+    // Dimensione dell'albero
+    const int treeSize = 4;
+
+    // Inizializza la matrice con dimensione treeSize x treeSize
+    std::vector<std::vector<int>> matrix {
+        std::vector<int> {0, 0, 0, 0},
+        std::vector<int> {0, 0, 0, 0},
+        std::vector<int> {0, 0, 0, 0},
+        std::vector<int> {0, 0, 0, 0}
+    };
+
+    // Chiama la tua funzione
+    minAntCom(root, matrix);
+
+    // Stampa la matrice risultante
+    std::cout << "Matrice dei minimi antenati comuni:" << std::endl;
+    for (int i = 0; i < treeSize; ++i) {
+        for (int j = 0; j < treeSize; ++j) {
+            std::cout << "Radice tra" << i << " e " << j << ": " << matrix[i][j] << " ";
+            std::cout << std::endl;
+        }
+    }
+
+    // Deallocazione della memoria dell'albero
+    // TODO: Aggiungi una funzione per la deallocazione della memoria degli elementi dell'albero
+
+    return 0;
+}
